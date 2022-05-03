c2e9d8097a964a348bc9bad52b7e7a48ac8893ea
isa wells <isabelwells@my.unt.edu>
2022-05-03 17:21:31 -0500

    edit class modal removed

--- toolbox-frontend/src/Components/ClassButton/ClassButton.js
+++ toolbox-frontend/src/Components/ClassButton/ClassButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Tutorial from '../../Images/tutorial.png'
 import '../Dashboard/Dashboard.css';
-import Modal from "../Modal/EditClassModal";
+
 
 
 const ClassButton = (props) => {
@@ -11,7 +11,7 @@ const ClassButton = (props) => {
   const navigate = useNavigate();
   return (
       <div className="Class-box" onClick={() => navigate("/classdashboard")}>
-            <Modal >name="EditClassModal";</Modal>
+
         <div className="Class-title">
           {props.className}
         </div>
